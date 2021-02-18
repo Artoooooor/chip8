589ca171befff1cad4bdd87dbe6ed462e9dad9c4
Artoooooor <artoooooor@gmail.com>
2021-02-18 20:32:14 +0100

    partial fix of linter errors

--- chip8/chip8cpu.py
+++ chip8/chip8cpu.py
@@ -1,23 +1,24 @@
 from math import floor
 
 CHIP8_STANDARD_FONT = [
-    0xF0, 0x90, 0x90, 0x90, 0xF0, 
-    0x20, 0x60, 0x20, 0x20, 0x70, 
-    0xF0, 0x10, 0xF0, 0x80, 0xF0, 
-    0xF0, 0x10, 0xF0, 0x10, 0xF0, 
-    0x90, 0x90, 0xF0, 0x10, 0x10, 
-    0xF0, 0x80, 0xF0, 0x10, 0xF0, 
-    0xF0, 0x80, 0xF0, 0x90, 0xF0, 
-    0xF0, 0x10, 0x20, 0x40, 0x40, 
-    0xF0, 0x90, 0xF0, 0x90, 0xF0, 
-    0xF0, 0x90, 0xF0, 0x10, 0xF0, 
-    0xF0, 0x90, 0xF0, 0x90, 0x90, 
-    0xE0, 0x90, 0xE0, 0x90, 0xE0, 
-    0xF0, 0x80, 0x80, 0x80, 0xF0, 
-    0xE0, 0x90, 0x90, 0x90, 0xE0, 
-    0xF0, 0x80, 0xF0, 0x80, 0xF0, 
+    0xF0, 0x90, 0x90, 0x90, 0xF0,
+    0x20, 0x60, 0x20, 0x20, 0x70,
+    0xF0, 0x10, 0xF0, 0x80, 0xF0,
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,
+    0x90, 0x90, 0xF0, 0x10, 0x10,
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,
+    0xF0, 0x10, 0x20, 0x40, 0x40,
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,
+    0xF0, 0x90, 0xF0, 0x90, 0x90,
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,
+    0xF0, 0x80, 0x80, 0x80, 0xF0,
+    0xE0, 0x90, 0x90, 0x90, 0xE0,
+    0xF0, 0x80, 0xF0, 0x80, 0xF0,
     0xF0, 0x80, 0xF0, 0x80, 0x80
 ]
+
 
 class Chip8State:
     def __init__(self):
@@ -37,9 +38,9 @@ class Chip8State:
 
     def load_program(self, program):
         rest = len(self.memory) - len(program) - 0x200
-        self.memory[0x200 : 0x200 + len(program)] = program
+        self.memory[0x200: 0x200 + len(program)] = program
         self.memory[0x200 + len(program):] = [0x00] * rest
-    
+
     def load_font(self, font):
         self.memory[:0x50] = font
 
@@ -54,14 +55,17 @@ class Chip8State:
         self.stack[:] = [0] * len(self.stack)
         self.load_font(CHIP8_STANDARD_FONT)
 
+
 class Chip8Cpu:
-    def __init__(self,state, rng):
-        self.state=state
+    def __init__(self, state, rng):
+        self.state = state
         self.rng = rng
         self.schip = False
 
     def tick(self):
-        instruction = (self.state.memory[self.state.PC] << 8) + self.state.memory[self.state.PC+1]
+        byte1 = self.state.memory[self.state.PC]
+        byte2 = self.state.memory[self.state.PC + 1]
+        instruction = (byte1 << 8) + byte2
         group = (instruction & 0xf000) >> 0x0c
         register1 = (instruction & 0x0f00) >> 0x08
         register2 = (instruction & 0x00f0) >> 0x04
@@ -70,28 +74,35 @@ class Chip8Cpu:
         mode = instruction & 0x000f
         halted = False
         if instruction == 0x00e0:
-            self.state.memory[-0x100:] = [0]*0x100
+            self.state.memory[-0x100:] = [0] * 0x100
         elif instruction == 0x00ee:
             self.state.PC = self.pop()
         elif group == 0x1:
-            self.state.PC = address - 2;
+            self.state.PC = address - 2
         elif group == 0x2:
             self.push(self.state.PC)
-            self.state.PC = address - 2;
+            self.state.PC = address - 2
         elif group == 0x3:
             self.skip_if_equal(self.state.registers[register1], value)
         elif group == 0x4:
             self.skip_if_not_equal(self.state.registers[register1], value)
         elif group == 0x5 and instruction & 0x000f == 0x00:
-            self.skip_if_equal(self.state.registers[register1], self.state.registers[register2])
+            self.skip_if_equal(
+                self.state.registers[register1],
+                self.state.registers[register2]
+            )
         elif group == 0x6:
             self.state.registers[register1] = value
         elif group == 0x7:
-            self.state.registers[register1] = (self.state.registers[register1] + value) & 0xff
+            self.state.registers[register1] = (
+                self.state.registers[register1] + value) & 0xff
         elif group == 0x8:
             self.handle_alu(register1, register2, mode)
         elif group == 0x9 and instruction & 0x000f == 0:
-            self.skip_if_not_equal(self.state.registers[register1], self.state.registers[register2])
+            self.skip_if_not_equal(
+                self.state.registers[register1],
+                self.state.registers[register2]
+            )
         elif group == 0xa:
             self.state.I = address
         elif group == 0xb:
@@ -104,21 +115,20 @@ class Chip8Cpu:
             self.handle_keyboard(register1, value)
         elif group == 0xf:
             halted = self.handle_memory_operation(register1, value)
-        
+
         if not halted:
             self.state.PC += 2
             self.handle_timers()
-        
 
     def push(self, number):
-        self.state.stack[self.state.SP]=number
+        self.state.stack[self.state.SP] = number
         self.state.SP += 1
 
     def pop(self):
-        number = self.state.stack[self.state.SP-1]
+        number = self.state.stack[self.state.SP - 1]
         self.state.SP -= 1
         return number
-    
+
     def skip_if_equal(self, value1, value2):
         if value1 == value2:
             self.state.PC += 2
@@ -155,31 +165,33 @@ class Chip8Cpu:
 
     def subtract(self, num1, num2):
         result = num1 - num2
-        self.state.registers[0xf] = 0x00 if result<0 else 0x01
-        if result<0:
+        self.state.registers[0xf] = 0x00 if result < 0 else 0x01
+        if result < 0:
             result += 0x100
         return result
 
     def add(self, num1, num2):
-        result = (num1+num2) & 0xff
+        result = (num1 + num2) & 0xff
         self.state.registers[0xf] = 0x01 if result < num1 else 0x00
         return result
 
     def draw(self, register1, register2, mode):
         x = self.state.registers[register1] & 0x3f
         y = self.state.registers[register2] & 0x1f
-        x_byte_shift = x >> 3;
-        y_byte_shift = y << 3;
-        shift_in_byte = x & 0x07;
+        x_byte_shift = x >> 3
+        y_byte_shift = y << 3
+        shift_in_byte = x & 0x07
         height = min(mode, 0x20 - y)
         start = self.state.screen_buffer_start + x_byte_shift + y_byte_shift
         self.state.registers[0xf] = 0x00
         for i in range(height):
-            sprite_byte=self.state.memory[self.state.I + i]
+            sprite_byte = self.state.memory[self.state.I + i]
             row_byte_shift = i << 3
-            self.draw_byte(start + row_byte_shift, sprite_byte >> shift_in_byte)
+            self.draw_byte(start + row_byte_shift,
+                           sprite_byte >> shift_in_byte)
             if x < 0x38:
-                self.draw_byte(start + row_byte_shift + 1, sprite_byte << (8-shift_in_byte) & 0xff)
+                self.draw_byte(start + row_byte_shift + 1,
+                               sprite_byte << (8 - shift_in_byte) & 0xff)
 
     def draw_byte(self, address, byte):
         self.state.memory[address] = self.state.memory[address] ^ byte
@@ -192,7 +204,7 @@ class Chip8Cpu:
             self.state.PC += 2
         elif mode == 0xa1 and not self.is_key_pressed(key):
             self.state.PC += 2
-    
+
     def handle_memory_operation(self, register, mode):
         halted = False
         registerValue = self.state.registers[register]
@@ -213,17 +225,19 @@ class Chip8Cpu:
         elif mode == 0x29:
             self.state.I = registerValue * 5
         elif mode == 0x33:
-            self.state.memory[self.state.I : self.state.I + 3] = to_bcd(registerValue)
+            self.state.memory[self.state.I: self.state.I + 3] = to_bcd(registerValue)
         elif mode == 0x55:
-            self.state.memory[self.state.I : self.state.I + register + 1] = self.state.registers[:register + 1]
+            length = register + 1
+            self.state.memory[self.state.I: self.state.I + length] = self.state.registers[:length]
             self.state.I += register + 1
         elif mode == 0x65:
-            self.state.registers[:register + 1] = self.state.memory[self.state.I : self.state.I + register + 1]
+            length = register + 1
+            self.state.registers[:length] = self.state.memory[self.state.I: self.state.I + length]
             self.state.I += register + 1
         return halted
-    
+
     def handle_timers(self):
-        if self.state.timer_counter>0:
+        if self.state.timer_counter > 0:
             self.state.timer_counter -= 1
         else:
             self.state.timer_counter = 9
@@ -237,15 +251,16 @@ class Chip8Cpu:
         return None
 
     def is_key_pressed(self, key):
-        if key<=0xf:
+        if key <= 0xf:
             return self.state.keys[key]
         else:
             return False
 
+
 def to_bcd(number):
-    a = floor(number/100)
+    a = floor(number / 100)
     number -= a * 100
-    b = floor(number/10)
+    b = floor(number / 10)
     number -= b * 10
     c = number
-    return a,b,c
+    return a, b, c
--- chip8/config.py
+++ chip8/config.py
@@ -1,6 +1,12 @@
 import pygame
-numbers_per_line = ((0x1, 0x2, 0x3, 0xC), (0x4, 0x5, 0x6, 0xD),
-                    (0x7, 0x8, 0x9, 0xE), (0xA, 0x0, 0xB, 0xF), ('step',), ('reset',))
+numbers_per_line = (
+    (0x1, 0x2, 0x3, 0xC),
+    (0x4, 0x5, 0x6, 0xD),
+    (0x7, 0x8, 0x9, 0xE),
+    (0xA, 0x0, 0xB, 0xF),
+    ('step',),
+    ('reset',)
+)
 
 
 def get_keys_config(lines, default):
@@ -9,14 +15,12 @@ def get_keys_config(lines, default):
         for i, line in enumerate(lines):
             chars = [char for char in line.split() if len(char) > 0]
             if len(chars) != len(numbers_per_line[i]):
-                print('Line {} \'{}\' has invalid length - use exactly {} entries'.format(
-                    i+1, line, len(numbers_per_line[i])))
+                print('Line {} \'{}\' has invalid length - use exactly {} entries'.format(i + 1, line, len(numbers_per_line[i])))
                 return default
             for (char, number) in zip(chars, numbers_per_line[i]):
                 key = getattr(pygame, 'K_' + char)
                 if key in keys:
-                    print('Duplicate key {} in line {} of config \'{}\''.format(
-                        char, i+1, line))
+                    print('Duplicate key {} in line {} of config \'{}\''.format(char, i + 1, line))
                     return default
                 keys[key] = number
 
