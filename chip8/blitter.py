463f35dbcd9acc8b8d82c320178f04e3b09071e7
Artoooooor <artoooooor@gmail.com>
2021-02-22 20:40:11 +0100

    Fixed whitespace and line length in blitter.py

--- chip8/blitter.py
+++ chip8/blitter.py
@@ -4,5 +4,5 @@ def blit_screen(state, color1, color2, outArray):
             index = (y << 3) + (x >> 3) + state.screen_buffer_start
             byte = state.memory[index]
             for i in range(0x08):
-                outArray[x+i, y] = color1 if (byte &
-                                              (0x80 >> i)) == 0 else color2
+                pixel = byte & (0x80 >> i)
+                outArray[x + i, y] = color1 if pixel == 0 else color2
